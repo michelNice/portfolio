dde8232f1ae4f886dfe32b7f8a2ca07202abb5c1
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-01-23 08:29:46 -0300

    update

--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@ const mobileMenu = document.getElementById('mobile-menu');
 const menuIcon = document.getElementById('menu-icon');
 
 mobileMenuBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
+  mobileMenu.classList.toggle('translate-x-full');
+  mobileMenu.classList.toggle('opacity-0');
+
   menuIcon.classList.toggle('fa-bars');
   menuIcon.classList.toggle('fa-xmark');
 });
 
 const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
+
 mobileNavLinks.forEach(link => {
   link.addEventListener('click', () => {
-    mobileMenu.classList.add('hidden');
+    mobileMenu.classList.add('translate-x-full');
+    mobileMenu.classList.add('opacity-0');
+
     menuIcon.classList.add('fa-bars');
     menuIcon.classList.remove('fa-xmark');
   });
