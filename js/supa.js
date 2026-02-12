e32c22b04c631cb726d8c49400e38b1f48f97409
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-02-12 08:14:59 -0300

    update

--- js/supa.js
+++ js/supa.js
@@ -1,25 +1,29 @@
-import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = 'https://qtphhhtqstcphwcnewdc.supabase.co'
 const supabaseKey = 'sb_publishable_diJO977_qHHwD6dvn1CflA_9JzkgsTx'
+const supabaseClient = window.supabase.createClient(
+   supabaseUrl,
+   supabaseKey
 
-const supabaseClient = createClient(supabaseUrl, supabaseKey)
+);
 
-export async function test(lang) {
+export async function test(lang){
 
-  const { data, error } = await supabaseClient
+    const {data,error} = await supabaseClient
     .from('translations')
-    .select('key, value')
-    .eq('lang', lang)
+    .select('key,value').eq('lang', lang)
+    
 
-  if (error) {
-    throw error
-  }
 
-  const result = data.reduce((acc, obj) => {
-    acc[obj.key] = obj.value
-    return acc
-  }, {})
+   const result = data.reduce((accumulator,obj)=> {
+    accumulator[obj.key] = obj.value
 
-  return result
+    return accumulator
+   }, {});
+
+   console.log(result)
+   console.log('Error',error)
+
+   return result
 }
+
+
